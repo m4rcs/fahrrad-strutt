ad44234eb703365a641a62e9d83e655f2118610a
Marc Schiller <m4rc.schiller@gmail.com>
2018-11-11 14:41:30 +0100

    Prepare for SCSS.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/app.js',
   output: {
-    filename: 'main.js',
+    filename: 'app.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
