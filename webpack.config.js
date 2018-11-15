7600bb58d7edf75b713fb285b361d9c48737c51c
Marc Schiller <m4rc.schiller@gmail.com>
2018-11-15 19:21:29 +0100

    Moved openings into new section.

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
     compress: true,
-    port: 9000
+    port: 9000,
+    watchContentBase: true
   },
   module: {
     rules: [{
